b3ffa7497d2201f1bd80b31cc90759e4632dfc4f
Kevin Wolf <kwolf@redhat.com>
2012-06-06 13:53:18 +0200

    qcow2: Reading from areas not in L2 tables yet
    In preparation of delayed COW (i.e. completing the guest write request
    before the associated COWs have completed) we must make sure that after
    the guest data has written the new data is read back, even if the COW
    hasn't completed and the new cluster isn't linked in the L2 table yet.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -861,6 +861,8 @@ static coroutine_fn int qcow2_co_writev(BlockDriverState *bs,
         while (l2meta != NULL) {
             QCowL2Meta *next;
 
+            l2meta->is_written = true;
+
             ret = qcow2_alloc_cluster_link_l2(bs, l2meta);
             if (ret < 0) {
                 goto fail;
