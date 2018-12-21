66c3b1fe2b888cd95df8b6ccd01065b3d4619b04
Fabiano Rosas <farosas@linux.ibm.com>
2018-12-21 18:48:19 -0200

    target/ppc: support single stepping with KVM HV
    The hardware singlestep mechanism in POWER works via a Trace Interrupt
    (0xd00) that happens after any instruction executes, whenever MSR_SE =
    1 (PowerISA Section 6.5.15 - Trace Interrupt).
    
    However, with kvm_hv, the Trace Interrupt happens inside the guest and
    KVM has no visibility of it. Therefore, when the gdbstub uses the
    KVM_SET_GUEST_DEBUG ioctl to enable singlestep, KVM simply ignores it.
    
    This patch takes advantage of the Trace Interrupt to perform the step
    inside the guest, but uses a breakpoint at the Trace Interrupt handler
    to return control to KVM. The exit is treated by KVM as a regular
    breakpoint and it returns to the host (and QEMU eventually).
    
    Before signalling GDB, QEMU sets the Next Instruction Pointer to the
    instruction following the one being stepped and restores the MSR,
    SRR0, SRR1 values from before the step, effectively skipping the
    interrupt handler execution and hiding the trace interrupt breakpoint
    from GDB.
    
    This approach works with both of GDB's 'scheduler-locking' options
    (off, step).
    
    Note:
    
    - kvm_arch_emulate_singlestep happens after GDB asks for a single step,
      while the vcpus are stopped.
    
    - kvm_handle_singlestep executes after the step, during the handling
      of the Emulation Assist Interrupt (breakpoint).
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -885,6 +885,23 @@ static inline void powerpc_excp(PowerPCCPU *cpu, int excp_model, int excp)
     powerpc_set_excp_state(cpu, vector, new_msr);
 }
 
+target_ulong ppc_get_trace_int_handler_addr(CPUState *cs, bool mmu_on)
+{
+    PowerPCCPU *cpu = POWERPC_CPU(cs);
+    CPUPPCState *env = &cpu->env;
+    int ail = 0;
+    unsigned long offset = 0;
+
+    /* AIL is only used if translation is enabled */
+    if (mmu_on) {
+        ail = (env->spr[SPR_LPCR] & LPCR_AIL) >> LPCR_AIL_SHIFT;
+        if (ail)
+	    offset = 0xc000000000004000;
+    }
+
+    return env->excp_vectors[POWERPC_EXCP_TRACE] | offset;
+}
+
 void ppc_cpu_do_interrupt(CPUState *cs)
 {
     PowerPCCPU *cpu = POWERPC_CPU(cs);
