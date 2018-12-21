0006acc00e3fbc3382559b498c0fe5ed1eeeeaaa
Fabiano Rosas <farosas@linux.ibm.com>
2018-12-21 18:48:19 -0200

    target/ppc: support single stepping with KVM HV
    The hardware singlestep mechanism in POWER works via a Trace Interrupt
    (0xd00) that happens after any instruction executes, whenever MSR_SE =
    1 (PowerISA Section 6.5.15 - Trace Interrupt).
    
    However, with kvm_hv, the Trace Interrupt happens inside the guest and
    KVM has no visibility of it. Therefore, when the gdbstub uses the
    KVM_SET_GUEST_DEBUG ioctl to enable singlestep, KVM simply ignores it.
    
    This patch takes advantage of the Trace Interrupt to perform the step
    inside the guest, but uses a breakpoint at the Trace Interrupt handler
    to return control to KVM. The exit is treated by KVM as a regular
    breakpoint and it returns to the host (and QEMU eventually).
    
    Before signalling GDB, QEMU sets the Next Instruction Pointer to the
    instruction following the one being stepped and restores the MSR,
    SRR0, SRR1 values from before the step, effectively skipping the
    interrupt handler execution and hiding the trace interrupt breakpoint
    from GDB.
    
    This approach works with both of GDB's 'scheduler-locking' options
    (off, step).
    
    Note:
    
    - kvm_arch_emulate_singlestep happens after GDB asks for a single step,
      while the vcpus are stopped.
    
    - kvm_handle_singlestep executes after the step, during the handling
      of the Emulation Assist Interrupt (breakpoint).
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -440,6 +440,10 @@ typedef struct ppc_v3_pate_t {
 #define msr_ts   ((env->msr >> MSR_TS1)  & 3)
 #define msr_tm   ((env->msr >> MSR_TM)   & 1)
 
+#define srr1_ir ((env->spr[SPR_SRR1] >> MSR_IR) & 1)
+#define srr1_dr ((env->spr[SPR_SRR1] >> MSR_DR) & 1)
+#define srr1_se ((env->spr[SPR_SRR1] >> MSR_SE) & 1)
+
 #define DBCR0_ICMP (1 << 27)
 #define DBCR0_BRT (1 << 26)
 #define DBSR_ICMP (1 << 27)
@@ -1158,6 +1162,13 @@ struct CPUPPCState {
     uint32_t tm_vscr;
     uint64_t tm_dscr;
     uint64_t tm_tar;
+
+    /* Used for software single step */
+    target_ulong sstep_srr0;
+    target_ulong sstep_srr1;
+    target_ulong trace_handler_addr;
+    bool guest_singlestep;
+    bool pending_singlestep;
 };
 
 #define SET_FIT_PERIOD(a_, b_, c_, d_)          \
@@ -1251,6 +1262,7 @@ struct PPCVirtualHypervisorClass {
     OBJECT_GET_CLASS(PPCVirtualHypervisorClass, (obj), \
                      TYPE_PPC_VIRTUAL_HYPERVISOR)
 
+target_ulong ppc_get_trace_int_handler_addr(CPUState *cs, bool mmu_on);
 void ppc_cpu_do_interrupt(CPUState *cpu);
 bool ppc_cpu_exec_interrupt(CPUState *cpu, int int_req);
 void ppc_cpu_dump_state(CPUState *cpu, FILE *f, int flags);
@@ -1264,6 +1276,7 @@ int ppc_cpu_gdb_write_register_apple(CPUState *cpu, uint8_t *buf, int reg);
 void ppc_gdb_gen_spr_xml(PowerPCCPU *cpu);
 const char *ppc_gdb_get_dynamic_xml(CPUState *cs, const char *xml_name);
 #endif
+
 int ppc64_cpu_write_elf64_note(WriteCoreDumpFunction f, CPUState *cs,
                                int cpuid, void *opaque);
 int ppc32_cpu_write_elf32_note(WriteCoreDumpFunction f, CPUState *cs,
@@ -2219,6 +2232,12 @@ enum {
                         PPC2_FP_CVT_S64 | PPC2_TM | PPC2_PM_ISA206 | \
                         PPC2_ISA300)
 };
+
+#define OP_RFID 19
+#define XOP_RFID 18
+#define OP_MOV 31
+#define XOP_MFMSR 83
+#define XOP_MTSPR 467
 
 /*****************************************************************************/
 /*
