f831289935be71cc8055871bf1e8929e4f9379f4
Cédric Le Goater <clg@kaod.org>
2021-12-14 08:48:47 +0100

    ppc/pnv: Add an endianness property to the machine
    To change the default CPU endianness and start in Little-Endian mode
    instead of Big-Endian.
    
    Useful the run the microwatt test suite. That's why we change also the
    entry point.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- include/hw/ppc/pnv.h
+++ include/hw/ppc/pnv.h
@@ -234,6 +234,7 @@ struct PnvMachineState {
     PnvPnor      *pnor;
 
     hwaddr       fw_load_addr;
+    bool         little_endian;
 };
 
 PnvChip *pnv_get_chip(PnvMachineState *pnv, uint32_t chip_id);
