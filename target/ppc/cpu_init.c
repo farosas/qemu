62e2f2a4eae20ada8e44bbe6034f34af8279d4ed
Fabiano Rosas <farosas@linux.ibm.com>
2021-12-31 11:52:31 -0300

    target/ppc: 405: Add missing exception handlers
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- target/ppc/cpu_init.c
+++ target/ppc/cpu_init.c
@@ -1951,7 +1951,9 @@ static void init_excp_4xx_softmmu(CPUPPCState *env)
     env->excp_vectors[POWERPC_EXCP_EXTERNAL] = 0x00000500;
     env->excp_vectors[POWERPC_EXCP_ALIGN]    = 0x00000600;
     env->excp_vectors[POWERPC_EXCP_PROGRAM]  = 0x00000700;
+    env->excp_vectors[POWERPC_EXCP_FPU]      = 0x00000800;
     env->excp_vectors[POWERPC_EXCP_SYSCALL]  = 0x00000C00;
+    env->excp_vectors[POWERPC_EXCP_APU]      = 0x00000F20;
     env->excp_vectors[POWERPC_EXCP_PIT]      = 0x00001000;
     env->excp_vectors[POWERPC_EXCP_FIT]      = 0x00001010;
     env->excp_vectors[POWERPC_EXCP_WDT]      = 0x00001020;
