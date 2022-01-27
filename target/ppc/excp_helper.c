80e342173d1eb34078f4b24b8a3afe2a640744ca
Fabiano Rosas <farosas@linux.ibm.com>
2022-01-27 18:20:37 -0300

    target/ppc: booke: Watchdog Timer interrupt
    Remove the switch as this function applies to BookE only.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -555,7 +555,6 @@ static void powerpc_excp_booke(PowerPCCPU *cpu, int excp)
 {
     CPUState *cs = CPU(cpu);
     CPUPPCState *env = &cpu->env;
-    int excp_model = env->excp_model;
     target_ulong msr, new_msr, vector;
     int srr0, srr1;
 
@@ -708,14 +707,8 @@ static void powerpc_excp_booke(PowerPCCPU *cpu, int excp)
         break;
     case POWERPC_EXCP_WDT:       /* Watchdog timer interrupt                 */
         trace_ppc_excp_print("WDT");
-        switch (excp_model) {
-        case POWERPC_EXCP_BOOKE:
-            srr0 = SPR_BOOKE_CSRR0;
-            srr1 = SPR_BOOKE_CSRR1;
-            break;
-        default:
-            break;
-        }
+        srr0 = SPR_BOOKE_CSRR0;
+        srr1 = SPR_BOOKE_CSRR1;
         break;
     case POWERPC_EXCP_DTLB:      /* Data TLB error                           */
     case POWERPC_EXCP_ITLB:      /* Instruction TLB error                    */
