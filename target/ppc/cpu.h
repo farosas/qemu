07d48e1d703b3a5b9dca13f55d1c38288cd6df05
Fabiano Rosas <farosas@linux.ibm.com>
2022-02-14 18:23:33 -0300

    target/ppc: Move powerpc_excp_books into cpu_books.c
    Take powerpc_reset_wakeup and ppc_excp_apply_ail along because these
    are specific to BookS as well.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -185,6 +185,7 @@ void powerpc_excp_6xx(PowerPCCPU *cpu, int excp);
 void powerpc_excp_7xx(PowerPCCPU *cpu, int excp);
 void powerpc_excp_74xx(PowerPCCPU *cpu, int excp);
 void powerpc_excp_booke(PowerPCCPU *cpu, int excp);
+void powerpc_excp_books(PowerPCCPU *cpu, int excp);
 
 #define PPC_INPUT(env) ((env)->bus_model)
 
