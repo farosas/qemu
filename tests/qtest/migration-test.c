314f57c60d70b05a3399052ea7fe7e076335661a
Nikolay Borisov <nborisov@suse.com>
2022-10-28 13:39:14 +0300

    tests/qtest: migration-test: Add tests for fixed-ram file-based migration
    Add basic tests for 'fixed-ram' migration.
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- tests/qtest/migration-test.c
+++ tests/qtest/migration-test.c
@@ -1772,6 +1772,48 @@ static void test_precopy_file_offset_bad(void)
     test_precopy_common(&args);
 }
 
+static void *migrate_fixed_ram_start_live(QTestState *from, QTestState *to)
+{
+    migrate_set_capability(from, "fixed-ram", true);
+    migrate_set_capability(to, "fixed-ram", true);
+
+    return NULL;
+}
+
+static void *migrate_fixed_ram_start_nonlive(QTestState *from, QTestState *to)
+{
+    migrate_fixed_ram_start_live(from, to);
+
+    migrate_set_capability(from, "suspend", true);
+    migrate_set_capability(to, "suspend", true);
+
+    return NULL;
+}
+
+static void test_precopy_file_fixed_ram_live(void)
+{
+    g_autofree char *uri = g_strdup_printf("file:%s/migfile", tmpfs);
+    MigrateCommon args = {
+        .connect_uri = uri,
+        .listen_uri = "defer",
+        .start_hook = migrate_fixed_ram_start_live,
+    };
+
+    test_precopy_common(&args);
+}
+
+static void test_precopy_file_fixed_ram_nonlive(void)
+{
+    g_autofree char *uri = g_strdup_printf("file:%s/migfile", tmpfs);
+    MigrateCommon args = {
+        .connect_uri = uri,
+        .listen_uri = "defer",
+        .start_hook = migrate_fixed_ram_start_nonlive,
+    };
+
+    test_precopy_common(&args);
+}
+
 static void test_precopy_tcp_plain(void)
 {
     MigrateCommon args = {
@@ -2801,6 +2843,12 @@ int main(int argc, char **argv)
                    test_precopy_file_offset);
     qtest_add_func("/migration/precopy/file/offset/bad",
                    test_precopy_file_offset_bad);
+
+
+    qtest_add_func("/migration/precopy/file/fixed-ram/nonlive",
+                   test_precopy_file_fixed_ram_nonlive);
+    qtest_add_func("/migration/precopy/file/fixed-ram/live",
+                   test_precopy_file_fixed_ram_live);
 
 #ifdef CONFIG_GNUTLS
     qtest_add_func("/migration/precopy/unix/tls/psk",
