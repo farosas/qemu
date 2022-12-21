da75080fb253f8478737360c58695df373d5064f
Fabiano Rosas <farosas@suse.de>
2022-12-21 15:53:56 -0300

    target/arm: Extract cpustate list manipulation to a file
    This code doesn't need to be buried in helper.c. Let's move it to its
    own file to keep things cleaner.
    
    Code moved verbatim.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    ---
    Extracted from:
    https://lore.kernel.org/r/20210416162824.25131-15-cfontana@suse.de
    [RFC v14 14/80] target/arm: split cpregs from tcg/helper.c

--- target/arm/cpregs.h
+++ target/arm/cpregs.h
@@ -443,6 +443,14 @@ void arm_cp_write_ignore(CPUARMState *env, const ARMCPRegInfo *ri,
 uint64_t arm_cp_read_zero(CPUARMState *env, const ARMCPRegInfo *ri);
 
 /*
+ * default raw read/write of coprocessor register field,
+ * behavior if no other function defined, and not const.
+ */
+uint64_t raw_read(CPUARMState *env, const ARMCPRegInfo *ri);
+/* CPWriteFn that just writes the value to ri->fieldoffset */
+void raw_write(CPUARMState *env, const ARMCPRegInfo *ri, uint64_t value);
+
+/*
  * CPResetFn that does nothing, for use if no reset is required even
  * if fieldoffset is non zero.
  */
