045470e2366bb90494782de3b296882939b25387
Fabiano Rosas <farosas@suse.de>
2022-12-23 11:53:29 -0300

    target/arm: Move hflags code into the tcg directory
    The hflags are used only for TCG code, so introduce a new file
    hflags.c to keep that code.

--- target/arm/tcg-stubs.c
+++ target/arm/tcg-stubs.c
@@ -9,8 +9,18 @@
 
 #include "qemu/osdep.h"
 #include "cpu.h"
+#include "internals.h"
 
 void write_v7m_exception(CPUARMState *env, uint32_t new_exc)
 {
     g_assert_not_reached();
+}
+
+void arm_rebuild_hflags(CPUARMState *env)
+{
+    g_assert_not_reached();
+}
+
+void assert_hflags_rebuild_correctly(CPUARMState *env)
+{
 }
