570d2fd4f8aa31699047fdb0637be9c05e54006c
Fabiano Rosas <farosas@suse.de>
2023-03-15 12:20:16 -0300

    migration/multifd: Add outgoing QIOChannelFile support
    Allow multifd to open file-backed channels. This will be used when
    enabling the fixed-ram migration stream format which expects a
    seekable transport.
    
    The QIOChannel read and write methods will use the preadv/pwritev
    versions which don't update the file offset at each call so we can
    reuse the fd without re-opening for every channel.
    
    Contrary to the socket migration, the file migration doesn't need an
    asynchronous channel creation process, so expose
    multifd_channel_connect() and call it directly.
    
    Note that this is just setup code and multifd cannot yet make use of
    the file channels.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/multifd.c
+++ migration/multifd.c
@@ -17,6 +17,7 @@
 #include "exec/ramblock.h"
 #include "qemu/error-report.h"
 #include "qapi/error.h"
+#include "file.h"
 #include "ram.h"
 #include "migration.h"
 #include "migration-stats.h"
@@ -28,6 +29,7 @@
 #include "threadinfo.h"
 #include "options.h"
 #include "qemu/yank.h"
+#include "io/channel-file.h"
 #include "io/channel-socket.h"
 #include "yank_functions.h"
 
@@ -705,6 +707,7 @@ static bool multifd_send_cleanup_channel(MultiFDSendParams *p, Error **errp)
 {
     if (p->c) {
         migration_ioc_unregister_yank(p->c);
+        qio_channel_close(p->c, NULL);
         object_unref(OBJECT(p->c));
         p->c = NULL;
     }
@@ -726,6 +729,7 @@ static bool multifd_send_cleanup_channel(MultiFDSendParams *p, Error **errp)
 
 static void multifd_send_cleanup_state(void)
 {
+    file_cleanup_outgoing_migration();
     socket_cleanup_outgoing_migration();
     qemu_sem_destroy(&multifd_send_state->channels_created);
     qemu_sem_destroy(&multifd_send_state->channels_ready);
@@ -988,7 +992,7 @@ static bool multifd_tls_channel_connect(MultiFDSendParams *p,
     return true;
 }
 
-static void multifd_channel_connect(MultiFDSendParams *p, QIOChannel *ioc)
+void multifd_channel_connect(MultiFDSendParams *p, QIOChannel *ioc)
 {
     qio_channel_set_delay(ioc, false);
 
@@ -1056,9 +1060,14 @@ out:
     error_free(local_err);
 }
 
-static void multifd_new_send_channel_create(gpointer opaque)
+static bool multifd_new_send_channel_create(gpointer opaque, Error **errp)
 {
+    if (!multifd_use_packets()) {
+        return file_send_channel_create(opaque, errp);
+    }
+
     socket_send_channel_create(multifd_new_send_channel_async, opaque);
+    return true;
 }
 
 bool multifd_send_setup(void)
@@ -1107,7 +1116,10 @@ bool multifd_send_setup(void)
         p->page_size = qemu_target_page_size();
         p->page_count = page_count;
         p->write_flags = 0;
-        multifd_new_send_channel_create(p);
+
+        if (!multifd_new_send_channel_create(p, &local_err)) {
+            return -1;
+        }
     }
 
     /*
