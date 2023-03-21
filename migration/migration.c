111df0be984d6170008016944e79d45221806894
Fabiano Rosas <farosas@suse.de>
2023-03-21 11:17:11 -0300

    migration/multifd: Add support to the fixed-ram stream format
    The new fixed-ram stream format uses a file transport and puts ram
    pages in the migration file at their respective offsets and can be
    done in parallel by using the pwritev system call which takes iovecs
    and an offset.
    
    Add support to enabling the new format along with multifd to make use
    of the threading and page handling already in place.
    
    This requires multifd to stop sending headers and leaving the stream
    format to the fixed-ram code. When it comes time to write the data, we
    need to call a version of qio_channel_write that can take an offset.
    
    Note: this implements outgoing migration only. Usage on HMP is:
    
    (qemu) stop
    (qemu) migrate_set_capability multifd on
    (qemu) migrate_set_capability fixed-ram on
    (qemu) migrate_set_parameter max-bandwidth 0
    (qemu) migrate_set_parameter multifd-channels 8
    (qemu) migrate file:migfile
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    ---
    For this RFC I'm doing incoming with no multifd:
    
    (qemu) migrate_set_parameter max-bandwidth 0
    (qemu) migrate_set_capability fixed-ram on
    (qemu) migrate_incoming file:migfile

--- migration/migration.c
+++ migration/migration.c
@@ -1343,11 +1343,6 @@ static bool migrate_caps_check(bool *cap_list,
 #endif
 
     if (cap_list[MIGRATION_CAPABILITY_FIXED_RAM]) {
-        if (cap_list[MIGRATION_CAPABILITY_MULTIFD]) {
-            error_setg(errp, "Directly mapped memory incompatible with multifd");
-            return false;
-        }
-
         if (cap_list[MIGRATION_CAPABILITY_XBZRLE]) {
             error_setg(errp, "Directly mapped memory incompatible with xbzrle");
             return false;
