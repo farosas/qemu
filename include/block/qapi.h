1c1be5bd5aa3aabdc15fd1596ee31c4ac2be084c
Fabiano Rosas <farosas@suse.de>
2023-06-07 18:13:47 -0300

    block: Convert bdrv_block_device_info into co_wrapper
    We're converting callers of bdrv_get_allocated_file_size() to run in
    coroutines because that function will be made asynchronous when called
    (indirectly) from the QMP dispatcher.
    
    This function is a candidate because it calls bdrv_query_image_info()
    -> bdrv_do_query_node_info() -> bdrv_get_allocated_file_size().
    
    It is safe to turn this is a coroutine because the code it calls is
    made up of either simple accessors and string manipulation functions
    [1] or it has already been determined to be safe [2].
    
    1) bdrv_refresh_filename(), bdrv_is_read_only(),
       blk_enable_write_cache(), bdrv_cow_bs(), blk_get_public(),
       throttle_group_get_name(), bdrv_write_threshold_get(),
       bdrv_query_dirty_bitmaps(), throttle_group_get_config(),
       bdrv_filter_or_cow_bs(), bdrv_skip_implicit_filters()
    
    2) bdrv_do_query_node_info() (see previous commit);
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- include/block/qapi.h
+++ include/block/qapi.h
@@ -30,10 +30,15 @@
 #include "block/snapshot.h"
 #include "qapi/qapi-types-block-core.h"
 
-BlockDeviceInfo * GRAPH_RDLOCK
-bdrv_block_device_info(BlockBackend *blk, BlockDriverState *bs,
-                       bool flat, Error **errp);
-
+BlockDeviceInfo *coroutine_fn GRAPH_RDLOCK
+bdrv_co_block_device_info(BlockBackend *blk,
+                          BlockDriverState *bs,
+                          bool flat,
+                          Error **errp);
+BlockDeviceInfo *co_wrapper bdrv_block_device_info(BlockBackend *blk,
+                                                   BlockDriverState *bs,
+                                                   bool flat,
+                                                   Error **errp);
 int GRAPH_RDLOCK
 bdrv_query_snapshot_info_list(BlockDriverState *bs,
                               SnapshotInfoList **p_list,
