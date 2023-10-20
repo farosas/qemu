b901fe55d64252277cc636e9c3a8a544ae8cf52c
Fabiano Rosas <farosas@suse.de>
2023-10-20 14:29:18 -0300

    multifd: Remove p->normal from source
    p->normal and p->normal_num are exactly the same as p->pages->offset
    and p->pages->num, respectively. Remove the redundant structures.
    
    This is not just a cleanup, but a step towards defining an opaque
    payload for multifd. The rationale is that the multifd channel should
    not have knowledge of the type of data it is transmitting (in this
    case a page).
    
    The following patches will isolate the handling of pages to either the
    client code (ram.c) or the compression hooks, making it easier to
    introduce generic data types to handle other kinds of data.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/multifd.h
+++ migration/multifd.h
@@ -124,10 +124,6 @@ typedef struct {
     struct iovec *iov;
     /* number of iovs used */
     uint32_t iovs_num;
-    /* Pages that are not zero */
-    ram_addr_t *normal;
-    /* num of non zero pages */
-    uint32_t normal_num;
     /* used for compression methods */
     void *data;
 }  MultiFDSendParams;
@@ -207,4 +203,3 @@ typedef struct {
 void multifd_register_ops(int method, MultiFDMethods *ops);
 
 #endif
-
