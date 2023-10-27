01d32a855559efe23bd465bce0a73e0b31b06454
Fabiano Rosas <farosas@suse.de>
2023-10-27 10:13:29 -0300

    migration/multifd: Rename MultiFDSend|RecvParams::data to compress_data
    Use a more specific name for the compression data so we can use the
    generic for the multifd core code.
    
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/multifd.h
+++ migration/multifd.h
@@ -129,7 +129,7 @@ typedef struct {
     /* num of non zero pages */
     uint32_t normal_num;
     /* used for compression methods */
-    void *data;
+    void *compress_data;
 }  MultiFDSendParams;
 
 typedef struct {
@@ -186,7 +186,7 @@ typedef struct {
     /* num of non zero pages */
     uint32_t normal_num;
     /* used for de-compression methods */
-    void *data;
+    void *compress_data;
 } MultiFDRecvParams;
 
 typedef struct {
