14a220af26cf45888271367857c308a5b14ab509
Fabiano Rosas <farosas@suse.de>
2023-12-01 10:46:09 -0300

    migration/ram: Add direct-io support to precopy file migration
    When doing precopy to a file using the fixed-ram stream format, we
    could benefit from bypassing the page cache for the bulk of the RAM
    pages transfer since the host pages containing the guest pages that
    were written to the migration file are only touched again after a
    potentially long period of time.
    
    We cannot simply enable the O_DIRECT flag, however, because not all IO
    performed by the migration thread satisfies the alignment requirements
    of O_DIRECT. There are many small read & writes that add headers and
    synchronization flags to the stream, which at the moment are required
    to always be present.
    
    Fortunately, due to fixed-ram migration there is a discernible moment
    where only RAM pages are written to the migration file. Enable
    direct-io during that moment.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/ram.c
+++ migration/ram.c
@@ -2248,6 +2248,9 @@ static int ram_save_host_page(RAMState *rs, PageSearchStatus *pss)
     /* Update host page boundary information */
     pss_host_page_prepare(pss);
 
+    migration_direct_io_start(pss->pss_channel,
+                              FIXED_RAM_FILE_OFFSET_ALIGNMENT);
+
     do {
         page_dirty = migration_bitmap_clear_dirty(rs, pss->block, pss->page);
 
@@ -2281,6 +2284,7 @@ static int ram_save_host_page(RAMState *rs, PageSearchStatus *pss)
 
         if (tmppages < 0) {
             pss_host_page_finish(pss);
+            migration_direct_io_finish(pss->pss_channel);
             return tmppages;
         }
 
@@ -2288,6 +2292,7 @@ static int ram_save_host_page(RAMState *rs, PageSearchStatus *pss)
     } while (pss_within_range(pss));
 
     pss_host_page_finish(pss);
+    migration_direct_io_finish(pss->pss_channel);
 
     res = ram_save_release_protection(rs, pss, start_page);
     return (res < 0 ? res : pages);
