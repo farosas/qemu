ffcc9ebd624ece99fad9e72c449a2b8078225818
Fabiano Rosas <farosas@suse.de>
2024-02-22 17:11:04 -0300

    fixup! block: Convert bdrv_block_device_info into co_wrapper

--- include/block/qapi.h
+++ include/block/qapi.h
@@ -31,14 +31,11 @@
 #include "qapi/qapi-types-block-core.h"
 
 BlockDeviceInfo *coroutine_fn GRAPH_RDLOCK
-bdrv_co_block_device_info(BlockBackend *blk,
-                          BlockDriverState *bs,
-                          bool flat,
+bdrv_co_block_device_info(BlockBackend *blk, BlockDriverState *bs, bool flat,
                           Error **errp);
-BlockDeviceInfo *co_wrapper bdrv_block_device_info(BlockBackend *blk,
-                                                   BlockDriverState *bs,
-                                                   bool flat,
-                                                   Error **errp);
+BlockDeviceInfo *co_wrapper_bdrv_rdlock
+bdrv_block_device_info(BlockBackend *blk, BlockDriverState *bs, bool flat,
+                       Error **errp);
 int GRAPH_RDLOCK
 bdrv_query_snapshot_info_list(BlockDriverState *bs,
                               SnapshotInfoList **p_list,
